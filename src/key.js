7bbcc80c987db4a3e03e87b61d8d28a950e92241
camicasii <camicasii@hotmail.com>
2019-10-23 13:25:14 -0400

    debug 1

--- src/key.js
+++ src/key.js
@@ -1,13 +1,11 @@
 module.exports = {
-    remotemysql2:{
-        
+    remotemysql2:{        
         host:process.env.DB_HOST_R,
         user:"RWjfrMVQvV",
         database:"RWjfrMVQvV",
         password:"Zp5ir12ixZ",
-        port:"3306"
-        
-    }   
-    
+        port:"3306"        
+    }     ,
 
+    hola:{}
 }
